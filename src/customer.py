7eb3b5298230e5dcefee6e7799c7651c995a84ba
Brice-Bertho <74135661+Brice-Bertho@users.noreply.github.com>
2022-01-24 17:38:43 +0100

    Update test_customer_tdd.py and customer.py

--- src/customer.py
+++ src/customer.py
@@ -1,3 +1,5 @@
+import uuid
+
 from src import account
 # from src.bank import Bank
 from src.account import Account
@@ -10,9 +12,17 @@ class Customer:
         self.accounts = []
 
     def add_account(self, Account):
-        account = Account()
-        self.accounts = account
+        account_number = Account.numero_compte
+        self.accounts.append(Account(account_number))
 
-    # def remove_account(self):
-    #
-    # def can_be_remove(self):
+    def remove_account(self, Account):
+        account_number = Account.numero_compte
+        if(Customer.remove_account == 1):
+            self.accounts.remove(Account(account_number))
+
+    def can_be_remove(self):
+        for i in len(self.accounts):
+            if (self.accounts[i].account_balance == 0):
+                return 1
+            else:
+                return 0
--- tests/test_customer_tdd.py
+++ tests/test_customer_tdd.py
@@ -1,8 +1,10 @@
 import pytest
+import uuid
 
 from src.customer import Customer
 from src.bank import Bank
 from src.account import Account
+
 
 class TestCustomer():
 
@@ -10,5 +12,11 @@ class TestCustomer():
     def default_customer(self):
         return Customer('Brice')
 
-    def test_add_account(self, default_customer):
-        assert default_customer.name == 'Brice'
+    @pytest.fixture
+    def default_account(self):
+        return Account()
+
+    def test_add_account_to_customer(self, default_customer, default_account):
+        test_account_number = uuid.uuid4()
+        default_customer.add_account(test_account_number)
+        assert default_customer.accounts == test_account_number

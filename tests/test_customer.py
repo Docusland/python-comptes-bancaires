9eb57a15fcd98c850b84a875b58f51ae66912855
akk0ga <45255672+akk0ga@users.noreply.github.com>
2022-01-24 12:43:58 +0100

    feat/Customer_class: begin writing test for customer

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,10 +1,37 @@
 import unittest
+import pytest
+from src.Customer import Customer
 
 
+@pytest.mark.customer
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
+    SUPPOSED_NAME = 'astol'
+
+    @pytest.fixture
+    def default_customer(self) -> Customer:
+        """ default customer account """
+        return Customer(customer_name=self.SUPPOSED_NAME)
+
+    def test_customer_bank_name(self):
+        """ TODO test customer bank name"""
+        pass
+
+    def test_customer_name(self, default_customer: Customer):
+        """ test if the saving account match with the username given """
+        self.assertEqual(self.SUPPOSED_NAME, default_customer.get_name())
+
+    def test_customer_ca_name(self, default_customer: Customer):
+        """ test if the saving account match with the username given """
+        self.assertEqual(default_customer.get_name(), default_customer.get_ca_name())
+
+    def test_customer_sc_name(self, default_customer: Customer):
+        """ test if the saving account match with the username given """
+        self.assertEqual(default_customer.get_name(), default_customer.get_sc_name())
+
+    def test_customer_transfer(self, default_customer: Customer):
+        """ TODO test the transfer between two account for customer"""
+        pass
 
 if __name__ == '__main__':
     unittest.main()

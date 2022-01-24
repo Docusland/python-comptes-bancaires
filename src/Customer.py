28baac35e3310d14954099379b6c344f8ecf3ab4
akk0ga <45255672+akk0ga@users.noreply.github.com>
2022-01-24 14:23:56 +0100

    feat/Customer_class: update class Customer with the new diagram

--- src/Customer.py
+++ src/Customer.py
@@ -1,5 +1,7 @@
+from src.account import Account
 from src.account import CurrentAccount as Ca
 from src.account import SavingsAccount as Sa
+import uuid
 
 
 class Customer:
@@ -12,8 +14,12 @@ class Customer:
         # TODO implement bank
         # self.__bank: str = customer_name
 
-        self.__currentAccount: Ca = Ca(owner_name=self.__name)
-        self.__savingAccount: Sa = Sa(owner_name=self.__name)
+        # the current account will always be the first item
+        # and the saving account the second
+        self.accounts: list[Ca | Sa | Account] = [
+            Ca(owner_name=self.__name),
+            Sa(owner_name=self.__name),
+        ]
 
     def get_name(self) -> str:
         """ return customer name """
@@ -21,8 +27,30 @@ class Customer:
 
     def get_ca_name(self) -> str:
         """ return current account name """
-        return self.__currentAccount.owner_name
+        return self.accounts[0].owner_name
 
     def get_sc_name(self) -> str:
         """ return current account name """
-        return self.__savingAccount.owner_name
+        return self.accounts[1].owner_name
+
+    def add_account(self, account: Account):
+        """
+        add an account for the customer
+
+        :param account:
+        """
+        pass
+
+    def remove_account(self, account: Account):
+        """
+        remove an account for the customer
+
+        :param account:
+        """
+        pass
+
+    def can_be_removed(self):
+        """
+        check if an account can be removed
+        """
+        pass

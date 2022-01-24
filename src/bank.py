dce0edc979989453b4b1f492f3af6767d004aa9e
Mathias Thuillier <mathias.thuillier@epsi.fr>
2022-01-24 16:25:09 +0100

    Ajout du test qui permet de trouver le consumer de la liste des consumers d'une banque en fonction de son uuid

--- src/bank.py
+++ src/bank.py
@@ -13,12 +13,15 @@ class Bank():
         self.customers = []
 
     def add_customer(self, customer: Customer):
+        """Add customer in list of customers"""
         self.customers.append(customer)
 
     def remove_customer(self, customer: Customer):
+        """Remove customer in list of customers"""
         self.customers.remove(customer)
 
     def find_customer_by_account_uuid(self, account_uuid):
+        """Find customer with account uuid in list of customers"""
         client = None
         for customer in self.customers:
             for account in customer.listAccount:
@@ -27,6 +30,7 @@ class Bank():
         return client
 
     def find_customer_by_customer_name(self, name):
+        """Find customer with customer name in list of customers"""
         client = None
         for customer in self.customers:
             if customer.name == name:
@@ -34,6 +38,10 @@ class Bank():
         return client
 
     def inner_transfert(self, account_from: Account, account_to: Account, montant):
+        """
+        Verify if account_from can transfert ammount and Transfert money from account_from to account_to with ammount
+        return true if passed
+        """
         if account_from.account_balance >= montant:
             account_from.money_withdraw(100, True)
             account_to.money_transfer(100)
--- tests/test_bank_tdd.py
+++ tests/test_bank_tdd.py
@@ -20,18 +20,45 @@ class TestBank():
         return Customer("Client1", [CurrentAccount("Compte courant"), SavingsAccount("Livret A")])
 
     def test_bank_name(self, bank:Bank) -> None:
+        """
+        If bank name is equal CIC
+        """
         assert bank.name == "CIC"
 
     def test_add_customer_in_bank(self, bank:Bank, customer:Customer) -> None:
+        """
+        If customer add in list of customers in bank is customer
+        """
         bank.add_customer(customer)
         assert bank.customers[0] == customer
 
     def test_customer_in_bank(self,bank: Bank, customer:Customer) -> None:
+        """
+        If bank have one customer after add customer in customers list of bank
+        """
         bank.add_customer(customer)
         assert len(bank.customers) > 0
 
     def test_remove_customer_in_bank(self, bank:Bank, customer:Customer) ->None:
+        """
+        If can remove customer in customers list of bank
+        """
         bank.add_customer(customer)
         bank.remove_customer(customer)
         assert len(bank.customers) == 0
 
+    def test_find_customer_with_customer_name(self, bank:Bank, customer:Customer):
+        """
+        If exist customer in customers list of bank with customer name
+        """
+        bank.add_customer(customer)
+        assert bank.find_customer_by_customer_name("Client1") == customer
+
+    def test_find_customer_with_customer_uuid(self, bank:Bank, customer:Customer):
+        """
+        If exist customer in customers list of bank with uuid
+        """
+        uuid_customer = customer.listAccount[0].numero_compte
+        bank.add_customer(customer)
+
+        assert bank.find_customer_by_account_uuid(uuid_customer) == customer

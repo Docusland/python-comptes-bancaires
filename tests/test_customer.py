b690552a17dc8ddb96da175b05799478597bc36e
akk0ga <45255672+akk0ga@users.noreply.github.com>
2022-01-24 12:57:20 +0100

    feat/Customer_class: fix bug on test

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,10 +1,9 @@
-import unittest
 import pytest
 from src.Customer import Customer
 
 
 @pytest.mark.customer
-class MyTestCase(unittest.TestCase):
+class TestCustomer():
 
     SUPPOSED_NAME = 'astol'
 
@@ -19,19 +18,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_customer_name(self, default_customer: Customer):
         """ test if the saving account match with the username given """
-        self.assertEqual(self.SUPPOSED_NAME, default_customer.get_name())
+        assert self.SUPPOSED_NAME == default_customer.get_name()
 
     def test_customer_ca_name(self, default_customer: Customer):
         """ test if the saving account match with the username given """
-        self.assertEqual(default_customer.get_name(), default_customer.get_ca_name())
+        assert default_customer.get_name() == default_customer.get_ca_name()
 
     def test_customer_sc_name(self, default_customer: Customer):
         """ test if the saving account match with the username given """
-        self.assertEqual(default_customer.get_name(), default_customer.get_sc_name())
+        assert default_customer.get_name() == default_customer.get_sc_name()
 
     def test_customer_transfer(self, default_customer: Customer):
         """ TODO test the transfer between two account for customer"""
         pass
-
-if __name__ == '__main__':
-    unittest.main()

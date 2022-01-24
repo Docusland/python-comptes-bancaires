fbb6040b4460a357a09703debe4f53aeda0b0aef
Samuel Challe <samuel.challe@gmail.com>
2022-01-24 14:22:45 +0100

    update test_compte_epargne_tdd.py

--- tests/test_compte_epargne_tdd.py
+++ tests/test_compte_epargne_tdd.py
@@ -1,6 +1,7 @@
 import pytest
 from random import randrange
 from src.account import SavingsAccount
+from src.customer import Customer
 
 @pytest.mark.ce
 class TestCompteEpargne():
@@ -8,7 +9,8 @@ class TestCompteEpargne():
     @pytest.fixture
     def compte_epargne(self):
         """ Default CE Account. """
-        return SavingsAccount("Username")
+        customer = Customer('Sterenn Grace')
+        return SavingsAccount(customer)
 
     def test_CompteEpargne_a_un_solde_a_zero_par_defaut(self, compte_epargne:SavingsAccount) -> None:
         """ By default a newly created account should have 0 €. """

e1b6f14e1d1f3086ea929131b88ea7c06a1b7eed
akk0ga <45255672+akk0ga@users.noreply.github.com>
2022-01-24 15:32:59 +0100

    feat/Bank_class: write test and method for NOT removing bank customer

--- src/Bank.py
+++ src/Bank.py
@@ -12,10 +12,23 @@ class Bank:
     def get_name(self) -> str:
         return self.__name
 
+    def get_customers(self):
+        return self.__customers
+
     def add_customer(self, customer: Customer):
         """
         add customer in the bank
 
         :param customer:
         """
-        pass
+        self.__customers.append(customer)
+
+    def remove_customer(self, customer: Customer):
+        """
+        remove customer from the bank
+        """
+        for tested_customer in self.__customers:
+            if tested_customer == customer:
+                if tested_customer.can_be_removed():
+                    self.__customers.remove(tested_customer)
+                    break
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -15,10 +15,30 @@ class TestBank:
         return Bank(bank_name=self.SUPPOSED_BANK_NAME)
 
     @pytest.fixture
-    def default_customer(self) -> Customer:
-        """ default customer account """
-        return Customer(customer_name=self.SUPPOSED_NAME)
+    def default_customer_can_be_removed(self) -> Customer:
+        """ default customer account can be removed """
+        return Customer(customer_name=self.SUPPOSED_CUSTOMER_NAME)
+
+    @pytest.fixture
+    def default_customer_cant_be_removed(self) -> Customer:
+        """ default customer account cant be removed """
+        return Customer(customer_name=self.SUPPOSED_CUSTOMER_NAME)
 
     def test_bank_name(self, default_bank: Bank):
         """ test bank name """
         assert self.SUPPOSED_BANK_NAME == default_bank.get_name()
+
+    def test_add_customer(self, default_bank: Bank, default_customer: Customer):
+        """ test the add of a new customer in the bank """
+        supposed_customer_in_bank = 1
+        default_bank.add_customer(default_customer)
+
+        assert len(default_bank.get_customers()) == supposed_customer_in_bank
+
+    def test_remove_customer_not_removed(self, default_bank: Bank, default_customer_can_be_removed: Customer):
+        """ test the method dont removed the customer """
+        supposed_customer_in_bank = 1
+        default_bank.add_customer(default_customer_can_be_removed)
+        default_bank.remove_customer(default_customer_can_be_removed)
+
+        assert len(default_bank.get_customers()) == supposed_customer_in_bank

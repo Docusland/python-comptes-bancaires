ed521f70602f88d03b07302d6a685cd73dfa0b01
Samuel Challe <samuel.challe@gmail.com>
2022-01-24 17:32:36 +0100

    update customer.py

--- src/customer.py
+++ src/customer.py
@@ -1,6 +1,29 @@
 class Customer:
     def __init__(self, name: str):
         """ default constructor """
-        self.name = name
-        self.accounts = []
+        self.__name = name
+        self.__accounts = []
 
+    # a getter function
+    @property
+    def name(self):
+        return self.__name
+
+    # a setter function
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    # a getter function
+    @property
+    def accounts(self):
+        return self.__accounts
+
+    # a setter function
+    @accounts.setter
+    def accounts(self, accounts):
+        self.__accounts = accounts
+
+    def add_account(self, account) -> bool:
+        self.__accounts.append(account)
+        return True

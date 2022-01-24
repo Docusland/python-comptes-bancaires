29d5a61f5bc03808105eeb35e59ba10d46694a33
OraneOrveillon <orane.orveillon@outlook.fr>
2022-01-24 12:40:18 +0100

    Passing test create a customer with all params

--- src/customer.py
+++ src/customer.py
@@ -1,3 +1,7 @@
 class Customer:
-    def __init__(self, name):
+    # TODO typer les paramètres
+    def __init__(self, name, bank, current_account, saving_account):
         self._name = name
+        self._bank = bank
+        self._current_account = current_account
+        self._saving_account = saving_account
--- tests/test_customer_tdd.py
+++ tests/test_customer_tdd.py
@@ -1,11 +1,13 @@
 from src.customer import Customer
+from src.bank import Bank
+from src.account import Account, CurrentAccount, SavingsAccount
 from unittest import TestCase
 
 
 class TestCustomer(TestCase):
 
     def setUp(self):
-        self.customer = Customer('Sterenn Grace')
+        self.customer = Customer('Sterenn Grace', Bank('CA'), CurrentAccount('Sterenn Grace'), SavingsAccount('Sterenn Grace'))
 
     def test_nothing(self):
         pass

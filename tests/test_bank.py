a510a6b6cb768143271d53a3748d4ea5723cfe04
Samuel Challe <samuel.challe@gmail.com>
2022-01-24 15:18:33 +0100

    update test_bank.py

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -3,6 +3,7 @@
 """
 import pytest
 from src.bank import Bank
+from src.customer import Customer
 
 
 @pytest.mark.b
@@ -16,3 +17,7 @@ class TestBank():
 
     def test_b_add_new_bank_with_ing_name(self, bank: Bank) -> None:
         assert bank.name == 'ING'
+
+    def test_b_add_customer_in_ing_bank(self, bank: Bank) -> None:
+        customer = Customer('Sterenn Grace')
+        assert bank.add_customer(customer) == True
